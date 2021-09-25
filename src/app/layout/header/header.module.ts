dcb9a819fac55cae340d69926149784dc5ac54a2
DiegoHZ5 <safairus@gmail.com>
2021-09-25 13:13:52 -0500

    feat: WA-S2 project creation & nabar
    first project attempt
    first navbar attempt

---
+++ src/app/layout/header/header.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
+
+import {HeaderComponent} from './header.component';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [HeaderComponent]
+})
+export class HeaderModule {}
